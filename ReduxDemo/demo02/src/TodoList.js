6c27bc3a243879989c959f6b850868f03e5c7dde
2268849815 <2268849815@qq.com>
2020-04-27 22:58:16 +0800

    redux+hooks

--- ReduxDemo/demo02/src/TodoList.js
+++ ReduxDemo/demo02/src/TodoList.js
@@ -1,23 +1,58 @@
-import React, { Component } from 'react';
-import store from './store'
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState()
-  }
-  render() { 
+import React from 'react';
+import {connect} from 'react-redux'
+
+const TodoList = (props) => {
+  let { inputValue, inputChange, clickBtn, deleteItem, list } = props
     return ( 
       <div>
         <div>
-          <input value={this.state.inputValue}/>
-          <button>提交</button>
+          <input 
+          value={inputValue} 
+          onChange={inputChange}
+          />
+          <button onClick={clickBtn}>提交</button>
         </div>
         <ul>
-          <li>jspang</li>
+          {
+            list.map((item,index) => {
+              return (
+                <li key={index} onClick={()=>{deleteItem(index)}}>
+                  {item}
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
      );
+}
+const stateToProps = (state) => {
+  return {
+    inputValue: state.inputValue,
+    list: state.list
   }
 }
- 
-export default TodoList;
+
+const dispatchToProps = (dispatch) => { // 映射 共享状态
+  return {
+    inputChange(e) {
+      let action = {
+        type: 'change_input',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+    clickBtn () {
+      let action = { type: 'add_item' }
+      dispatch(action)
+    },
+    deleteItem (index) {
+      let action = {
+        type: 'delete_item',
+        index: index
+      }
+      dispatch(action)
+    }
+  }
+}
+export default connect(stateToProps,dispatchToProps)(TodoList);
